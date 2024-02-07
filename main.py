12d5ff9ff1ca6fb4e4508493cf2646513c95d16b
Colton-2000 <colton.french@students.fhu.edu>
2024-02-06 22:14:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(word1, word2):
+    if word1 == '' or word2 == '':
+        anagram = False
+        return anagram
+    anagram = True
+    letters1 = []
+    letters2 = []
+    word1.replace(' ','')
+    word2.replace(' ','')
+    for letter in word1:
+        letters1.append(letter)
+    for letter in word2:
+        letters2.append(letter)
+    for letter in letters1:
+        if letter in letters2:
+            continue
+        else:
+            anagram = False
+            break
+    for letter in letters2:
+        if letter in letters1:
+            continue
+        else:
+            anagram = False
+            break
+    return anagram
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    word1 = input()
+    word2 = input()
+    print(anagram(word1, word2))
     
